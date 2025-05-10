5b90e75261847f9dec3263b8921d55b87ba9dcb0
Dana <dana.0730@mail.ru>
2025-05-10 13:47:11 +0500

    Minor tailoring

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -4,17 +4,42 @@ import MonthlyStatsChart from "../components/MonthlyStatsChart";
 import { fetchTransactions } from "../redux/slices/transactionSlice";
 import { fetchCategories } from "../redux/slices/categorySlice";
 import CategoryStatsChart from "../components/CategoryStatsChart";
+import styles from "./CategorySettings.module.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../redux/slices/authSlice";
 const Stats = () => {
   const transactions = useSelector((state) => state.transactions.list || []);
     const categories = useSelector((state) => state.categories.items || []);
+    const navigate = useNavigate();
    const dispatch = useDispatch();
     useEffect(() => {
        dispatch(fetchTransactions());
        dispatch(fetchCategories());
      }, [dispatch]);
   console.log("transactions:", transactions);
+
+  const handleLogout = () => {
+        if (window.confirm("Вы уверены, что хотите выйти?")) {
+          dispatch(logout());
+          navigate("/login");
+        }
+      };
+
   return (
-    <div>
+    <div className={styles.CategoryConteiner}>
+      <div className={styles.greeting}>
+        <h4>Статистика</h4>
+        <div>
+          <Link to="/settings" className={styles.headerLink}>
+              Категории
+            </Link>
+            <Link to="/dashboard" className={styles.headerLink}>
+              Транзакции
+            </Link>
+            <button onClick={handleLogout} className={styles.logoutBtn}>Выйти</button>
+          </div>
+      </div>
       <MonthlyStatsChart transactions={transactions} />
       <CategoryStatsChart />
     </div>
